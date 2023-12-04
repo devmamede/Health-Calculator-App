0e3f1209cc426b6d49ceb2cb9acab4449a36e507
Isac <isac.mamede14@gmail.com>
2023-12-04 18:00:10 -0300

    added vibration on error

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Vibration,
+  Keyboard,
+} from "react-native";
 import ResultImc from "./ResultImc/index";
 import styles from "./style";
 
@@ -9,44 +16,57 @@ export default function Form() {
   const [messageImc, setMessageImc] = useState("Informe o seu peso e altura");
   const [imc, setImc] = useState(null);
   const [textButton, setTextButton] = useState("Calcular");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   function imcCalculator() {
     return setImc((weight / (height * height)).toFixed(2));
   }
 
+  function verificationImc() {
+    if (imc == null) {
+      setErrorMessage("Campo Obrigatório*");
+      Vibration.vibrate();
+    }
+  }
+
   function validationImc() {
+    verificationImc();
     if (weight != null && height != null) {
+      Keyboard.dismiss();
       imcCalculator();
       setHeight(null);
       setWeight(null);
       setMessageImc("Seu IMC é igual a:");
       setTextButton("Calcular novamente");
+      setErrorMessage(null);
       return;
+    } else {
+      setImc(null);
+      setTextButton("Calcular");
+      setMessageImc("Por favor, informe seu peso e sua altura");
     }
-    setImc(null);
-    setTextButton("Calcular");
-    setMessageImc("Por favor, informe seu peso e sua altura");
-    setTextButton("Calcular novamente");
   }
 
   return (
     <View style={styles.formContext}>
       <View style={styles.form}>
         <Text style={styles.formLabel}>Altura</Text>
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
         <TextInput
           onChangeText={setHeight}
           value={height}
           placeholder="Ex. 1.85"
-          keyboardType="numeric"
+          keyboardType="text"
           style={styles.input}
         />
 
         <Text style={styles.formLabel}>Peso</Text>
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
         <TextInput
           onChangeText={setWeight}
           value={weight}
           placeholder="Ex. 65.5"
-          keyboardType="numeric"
+          keyboardType="text"
           style={styles.input}
         />
 
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -50,6 +50,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#fff",
   },
+
+  errorMessage: {
+    fontSize: 12,
+    color: "red",
+    fontWeight: "bold",
+    paddingLeft: 20,
+  },
 });
 
 export default styles;
